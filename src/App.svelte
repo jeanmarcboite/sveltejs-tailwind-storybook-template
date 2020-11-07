6b84726dec5b659a4f780ba07683991eda016e3d
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-11-07 17:37:15 +0100

    tailwind example

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,42 @@
 <script>
-	export let name;
+  import Tailwind from "./Tailwind.svelte";
+
+  export let name;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<style lang="postcss">
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+      .tw {
+        @apply font-sans text-lg text-center text-green-800;
+      }
+    }
+  }
 </style>
+
+<Tailwind />
+<main>
+  <h1>Hello {name}!</h1>
+  <p class="tw">tailwind included<span class="text-red-800">!</span></p>
+  <p>
+    Visit the
+    <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
+    to learn how to build Svelte apps.
+  </p>
+</main>
